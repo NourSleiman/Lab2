ffaee3b9552d54cef2fdb645fa1ca04e25616dae
NourSleiman <32310672+NourSleiman@users.noreply.github.com>
2021-10-12 17:13:15 -0400

    Update and rename movieCount.sh to DataAnalysis/movieCount.sh

--- DataAnalysis/movieCount.sh
+++ DataAnalysis/movieCount.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/bash
 
+#Data set from MovieLens from 1915 to 2018
 INPUT=./movies.csv 
+
+#An array of all the genres
 declare -a genres=("Action" "Adventure" "Animation" 
 "Children" "Comedy" "Crime" "Documentary" "Drama" "Fantasy" "Film-Noir"
 "Horror" "IMAX" "Musical" "Mystery" "Romance" "Sci-Fi" 
@@ -12,11 +15,13 @@ do
 	totalCount=`awk 'END { print NR }' ./totalMovies.txt`
 	echo "$i - Total Movies: $totalCount" >> ./movieDistribution.txt
 
+	#Filter out movies by their genres, do a count for each genre, and send it to genreCount.txt
 	for g in "${genres[@]}"
 	do
 		awk "/$g/ {print}" totalMovies.txt > ./genreCount.txt
 		count=`awk 'END { print NR }' ./genreCount.txt`
 		echo "$g : $count" >> ./movieDistribution.txt
 	done
+	#The comprehensive list for all years and the frequency of genres is printed to movieDistribution.txt
 	echo "----------------" >> ./movieDistribution.txt
 done
